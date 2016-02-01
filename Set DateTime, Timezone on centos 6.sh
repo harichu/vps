6fb5189dca38aed1cfa7466036f86fb5d49ae55f
harichu <chuhainet@gmail.com>
2016-02-01 14:33:45 +0700

    Update Set DateTime, Timezone on centos 6.sh

--- Set DateTime, Timezone on centos 6.sh
+++ Set DateTime, Timezone on centos 6.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Change Timezone in Linux
-mv /etc/localtime /etc/localtime.bk
-cp /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
+sudo mv /etc/localtime /etc/localtime.bk
+sudo cp /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 #Change Date and Time in Linux
 #date MMDDhhmmYYYY
 
 # Sync your system time and date with ntp (Network Time Protocol) over internet
-yum install -y ntp
-ntpdate pool.ntp.org
+sudo yum install -y ntp
+sudo ntpdate pool.ntp.org
